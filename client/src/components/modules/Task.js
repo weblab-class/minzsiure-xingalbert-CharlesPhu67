0a7c06d2114e22e98e34c0baa098e06fe138370a
Albert B Xing <axing@mit.edu>
2021-01-16 18:22:22 -0500

    added TaskBar functionality

--- client/src/components/modules/Task.js
+++ client/src/components/modules/Task.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
-import { Resizable } from "re-resizable";
+import { Draggable } from "react-beautiful-dnd";
 
 import "../../utilities.css";
 import "./Task.css";
+
+const grid = 8;
+const getItemStyle = (isDragging, draggableStyle, defaultBackgroundColor, width) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+    padding: grid * 2,
+    margin: `0 1px 0 0`,
+  
+    // change background colour if dragging
+
+    background: isDragging ? "lightgreen" : defaultBackgroundColor,
+    flexGrow: width,
+  
+    // styles we need to apply on draggables
+    ...draggableStyle
+  });
 
 class Task extends Component {
     constructor(props) {
@@ -14,17 +30,27 @@ class Task extends Component {
     }
 
     render() {
+        let isBreak = this.props.type === "break";
+        let defaultBackgroundColor = isBreak ? "#FFF4E9" : "#41CBC3";
         return (
-            <>
-                <Resizable className="Task-container"
-                    defaultSize={{
-                        width:320,
-                        height:200,
-                    }}
-                >
-                    Sample with default size
-                </Resizable>
-            </>
+            <Draggable key={this.props.id} draggableId={this.props.id} index={this.props.index}>
+                {(provided, snapshot) => (
+                    <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style,
+                            defaultBackgroundColor,
+                            this.props.duration
+                        )}
+                        className="Task-style"
+                    >
+                        {this.props.name}
+                    </div>
+                )}
+            </Draggable>
         )};
 }
 
--- client/src/components/modules/TaskBar.js
+++ client/src/components/modules/TaskBar.js
@@ -1,93 +1,108 @@
 import React, { Component } from "react";
 import Task from "./Task.js";
 import { Resizable } from "re-resizable";
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 import "../../utilities.css";
 import "./TaskBar.css";
 
-let TestTask = {
-    type: "Task",
-    name: "TestTask",
-    duration: 1,
-}
-let TestBreak = {
-    type: "Break",
-    name: "TestBreak",
-    duration: 1,
-}
-const style = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    border: 'solid 1px #ddd',
-    background: '#f0f0f0',
-  };
+const getDefaultTasks = () => ([
+	{
+		id: "task-1",
+		name: "Task 1",
+		type: "task",
+		duration: 15,
+	},
+	{ id: "break-1", name: "Break", type: "break", duration: 5, },
+	{ id: "task-2", name: "Task 2", type: "task", duration: 15, },
+	{ id: "break-2", name: "Break", type: "break", duration: 5, },
+	{ id: "task-3", name: "Task 3", type: "task", duration: 15, }
+]);
 
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  display: "flex",
+  padding: grid,
+  overflow: "auto"
+});
 
 class TaskBar extends Component {
     constructor(props) {
         super(props);
-        this.state= {
-            duration: 60,
-            tasks: [TestTask, TestBreak],
-        }
+        this.state = {
+          tasks: getDefaultTasks(),
+        };
+        this.onDragEnd = this.onDragEnd.bind(this);
     }
 
     componentDidMount() {
         //will need to implement api request
     }
 
+    onDragEnd(result) {
+        // dropped outside the list
+        if (!result.destination) {
+          	return;
+        }
+    
+        const tasks = reorder(
+			this.state.tasks,
+			result.source.index,
+			result.destination.index
+        );
+
+        this.setState({
+          	tasks
+        });
+      }
+    
+
     render() {
-        let tasksList=this.state.tasks.map((task) => (
-            <Task 
+		
+        let taskList=this.state.tasks.map((task, index) => (
+			<Task 
+				id={task.id}
+				index={index}
                 className="TaskBar-task"
-                type={task.type}
-                name={task}
+				name={task.name}
+				type={task.type}
                 duration={task.duration}
             />
         ))
-        console.log(tasksList);
+        console.log(taskList);
+        
+        
         return (
             <>
                 <div>
                     taskbar stuffs here
                 </div>
-                <div className="u-flexRow">
-                {/* <div> */}
-                    asdf
-                    
-                </div>
-                <div
-                    className="TaskBar-style"
-                    >
-                    <Resizable
-                        style={style}
-                        defaultSize={{
-                        width: '50%',
-                        height: 200,
-                        }}
-                        maxWidth="100%"
-                        minWidth="1"
-                    >
-                        001
-                    </Resizable>
-                    
-                    <Resizable
-                        style={style}
-                        defaultSize={{
-                        width: '50%',
-                        height: 200,
-                        }}
-                        maxWidth="100%"
-                        minWidth="1"
-                    >
-                        001
-                    </Resizable>
-                    {/* <div style={{ ...style, width: '100%', minWidth: '1px' }}>
-                        002
-                    </div> */}
-                </div>
+                <DragDropContext onDragEnd={this.onDragEnd}>
+					<Droppable droppableId="TaskBar" direction="horizontal">
+						{(provided, snapshot) => (
+							<div
+								ref={provided.innerRef}
+								style={getListStyle(snapshot.isDraggingOver)}
+								className="TaskBar-style"
+								{...provided.droppableProps}
+							>
+								{taskList}
+								{provided.placeholder}
+							</div>
+						)}
+       				</Droppable>
+				</DragDropContext>
             </>
         )};
 }
