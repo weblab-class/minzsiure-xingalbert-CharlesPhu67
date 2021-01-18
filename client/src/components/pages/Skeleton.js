9ee63dd6c89ac822209e300e588baadd4aef4022
CharlesPhu67 <71537756+CharlesPhu67@users.noreply.github.com>
2021-01-18 14:35:28 -0500

    Update Skeleton.js

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -6,7 +6,7 @@ import "../../utilities.css";
 import "./Skeleton.css";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "853392870845-01fp5j18t6rvnfcmg5mp4crvjuot6154.apps.googleusercontent.com";
 
 class Skeleton extends Component {
   constructor(props) {
